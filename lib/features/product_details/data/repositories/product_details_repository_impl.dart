8fb5f3ebdd7550f9fdc864f243e976c96a480cd8
Noa Tubic <ntubic@4ofthem.eu>
2024-12-04 14:57:30 +0100

    Added product details model, entity and mapper

--- lib/features/product_details/data/repositories/product_details_repository_impl.dart
+++ lib/features/product_details/data/repositories/product_details_repository_impl.dart
@@ -1,8 +1,12 @@
+import 'dart:developer';
+
 import 'package:either_dart/either.dart';
 import 'package:food_safety/core/data/error_resolvers/generic_error_resolver.dart';
 import 'package:food_safety/features/product_details/data/datasources/product_details_data_source.dart';
+import 'package:food_safety/features/product_details/data/mappers/product_details_mapper.dart';
 import 'package:food_safety/features/product_details/domain/entities/product_details.dart';
 import 'package:food_safety/features/product_details/domain/repositories/product_details_repository.dart';
+import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:q_architecture/q_architecture.dart';
 
 class ProductDetailsRepositoryImpl
@@ -16,6 +20,21 @@ class ProductDetailsRepositoryImpl
       execute(() async {
         final productResponse =
             await _productDetailsDataSource.getProductDetails(barcode);
-        return Right(ProductDetails());
+
+        ProductDetails productDetails =
+            mapProductToProductDetails(productResponse!);
+
+        log("Mapped Product Details:");
+        log("Barcode: ${productDetails.barcode}");
+        log("Name: ${productDetails.name}");
+        log("Generic Name: ${productDetails.genericName}");
+        log("Brand: ${productDetails.brand}");
+        log("Country: ${productDetails.country}");
+        log("Image URL: ${productDetails.imageUrl}");
+        log("Quantity: ${productDetails.quantity}");
+        log("Nutritional Values: ${productDetails.nutritionalValues?.map((e) => "${e.name}: ${e.value}").join(", ")}");
+        log("Ingredients: ${productDetails.ingredients?.join(", ")}");
+
+        return Right(productDetails);
       }, errorResolver: const GenericErrorResolver());
 }
--- lib/features/product_details/domain/entities/product_details.dart
+++ lib/features/product_details/domain/entities/product_details.dart
@@ -1,6 +1,30 @@
-import 'package:equatable/equatable.dart';
+class ProductDetails {
+  final String? barcode;
+  final String? name;
+  final String? genericName;
+  final String? brand;
+  final String? country;
+  final String? imageUrl;
+  final String? quantity;
+  final List<NutritionalValue>? nutritionalValues;
+  final List<String>? ingredients;
 
-class ProductDetails extends Equatable {
-  @override
-  List<Object?> get props => [];
+  ProductDetails({
+    this.barcode,
+    this.name,
+    this.genericName,
+    this.brand,
+    this.country,
+    this.imageUrl,
+    this.quantity,
+    this.nutritionalValues,
+    this.ingredients,
+  });
+}
+
+class NutritionalValue {
+  final String name;
+  final String value;
+
+  NutritionalValue({required this.name, required this.value});
 }
